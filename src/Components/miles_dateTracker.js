0fda2c9622e6b914e17757a69de4b81e9dc23f8d
Josh Hyink <joshuabhyink@gmail.com>
2021-02-01 15:58:38 -0700

    Started Front End

---
+++ src/Components/miles_dateTracker.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+
+class miles_dateTracker extends Component{
+    constructor(){
+        super()
+        
+    }
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default miles_dateTracker
